1ff2c7f16d3d92e82d468a768b2202671ba18ff4
rdarida <rdarida@users.noreply.github.com>
2021-04-02 19:50:54 +0200

    refactor: everything

--- __template__/test/index.test.ts
+++
@@ -1,7 +0,0 @@
-import log from '../src';
-
-describe('Test index', () => {
-  test('log should be truthy', () => {
-    expect(log).toBeTruthy();
-  });
-});
--- packages/base-scripts/src/cli.js
+++ packages/base-scripts/src/cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import log from './index';
+const log = require('./index');
 
 ((cwd, argv) => {
   try {
